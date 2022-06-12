178e0250069b3c852309ffd07dc519bd1dacd686
Ruslan Tokar <djrexxx87@gmail.com>
2022-06-12 14:57:57 +0300

    dynamic today Lesson Table on Dashboard

--- students/views.py
+++ students/views.py
@@ -5,10 +5,48 @@ def student_index(request):
     return redirect('/student/1/dashboard')
 
 def student_dashboard(request, sid):
-    return HttpResponse('<h1> Student DashBoard  %s</h1> ' %sid)
+    todayTimeTable = (
+        {'id' : 1,
+         'lessonName' : 'Англійська мова',
+         'lessonStartTime' : '08:30',
+         'lessonEndTime' : '09:15',
+         'ended' : True,
+         'ordered' : 1},
+        {'id' : 2,
+         'lessonName' : 'Фізкультура',
+         'lessonStartTime' : '09:25',
+         'lessonEndTime' : '10:10',
+         'ended' : True,
+         'ordered' : 2},
+        {'id' : 3,
+         'lessonName' : 'Математика',
+         'lessonStartTime' : '10:30',
+         'lessonEndTime' : '11:15',
+         'ended' : False,
+         'ordered' : 3},
+        {'id' : 4,
+         'lessonName' : 'Історія України',
+         'lessonStartTime' : '11:45',
+         'lessonEndTime' : '12:30',
+         'ended' : False,
+         'ordered' : 4},
+        {'id' : 5,
+         'lessonName' : 'Математика',
+         'lessonStartTime' : '12:45',
+         'lessonEndTime' : '13:30',
+         'ended' : False,
+         'ordered' : 5},
+    )
+    return render(request, 'studentDashboard.html', {'todayTimeTable' : todayTimeTable})
 
 def student_profile(request, sid):
     return HttpResponse('<h1> Student Profile %s </h1>' %sid)
 
 def student_class(request, sid):
     return HttpResponse('<h1> Student Class %s </h1>' %sid)
+
+
+'''
+def login(request):
+    return render(request, 'login.html', {})
+'''
